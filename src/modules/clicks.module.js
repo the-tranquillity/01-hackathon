1589537aaa428bf3b1ea92ba18258ca1d035d038
the-tranquillity <katyanov@protonmail.com>
2022-06-25 12:55:34 +0800

    Added basic logic to clicks feature

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,7 +1,64 @@
 import { Module } from '../core/module';
+import { statAddClick, addMultipleEventListener } from '../utils';
 
 export class ClicksModule extends Module {
-  trigger() {
-    console.log('trigger ClicksModule');
+  async trigger() {
+    const arrClicks = [];
+    addMultipleEventListener(document.body, ['click', 'dblclick'], (event) =>
+      statAddClick(event, arrClicks)
+    );
+
+    const response = async (waitMs) =>
+      new Promise((resolve) =>
+        setTimeout(() => resolve(callback(arrClicks)), waitMs)
+      );
+
+    const result = await response(4000);
+    return result;
+
+    function callback(arr) {
+      const dblAmount = arr.filter((el) => el.type === 'dblclick').length;
+      const clcAmount =
+        arr.filter((el) => el.type === 'click').length - dblAmount * 2;
+      console.log(
+        `Total clicks ${
+          arr.length - dblAmount
+        } (${clcAmount} single, ${dblAmount} double). Event log:`
+      );
+      console.table(arr);
+    }
   }
 }
+//#region Trash
+//console.log('trigger ClicksModule');
+//console.log(result);
+//const total = obj.length;
+//return console.log(total);
+/*     document.body.addEventListener('dblclick', (event) => {
+      objClicks[Date.now()] = includeClick(event);
+      console.log('dbl detail', event.detail);
+    });
+
+    document.body.addEventListener('click', (event) => {
+      objClicks[Date.now()] = includeClick(event);
+      console.log('single detail', event.detail);
+    });
+
+    (event) => {
+        objClicks[Date.now()] = ;
+        console.log('single detail', event.detail);
+ */
+/* document.body.addEventListener('click', (event) =>
+      statAddClick(event, objClicks)
+    ); */
+//const res = await setTimeout(() => objClicks, 5000);
+//return console.log('res', res);
+/*     const result = await x();
+
+    
+    
+
+    const x = async (waitMs) =>
+    new Promise((resolve) => setTimeout(() => resolve('done'), waitMs));
+    console.log(await x(1000)); */
+//#endregion
