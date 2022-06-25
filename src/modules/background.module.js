96aeb8a9a108efa65a222fe1b3ac20138169ca48
Radko Ksenia <ksradko16@yandex.ru>
2022-06-25 11:00:07 +0300

    init

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,6 +1,10 @@
 import { Module } from '../core/module';
 
+console.log('123')
+console.log('hello')
+
 export class BackgroundModule extends Module {
+
   trigger() {
     console.log('trigger BackgroundModule');
   }
