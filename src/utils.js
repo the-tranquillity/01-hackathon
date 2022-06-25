187bc1327ea56024c6c026b36e36736b50e1e556
the-tranquillity <katyanov@protonmail.com>
2022-06-25 12:51:33 +0800

    Added menu close before trigger

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,7 @@
+//#region Import
+import { OPEN_CLASS } from './globals';
+//#endregion
+
 //#region Classes
 export class ElementsFactory {
   static createInstances(data) {
@@ -23,10 +27,28 @@ export function handleMenuClick(event, menuModules, menuHTML) {
     event.target?.dataset?.type,
     event.target.offsetParent === menuHTML,
   ];
+
   // Возможно, проверка лишняя
   if (!datatype || !isMenuChild) return false;
 
   const module = Object.keys(menuModules).find((key) => key === datatype);
+  menuHTML.classList.remove(OPEN_CLASS);
   if (menuModules[module]) menuModules[module].trigger();
 }
+
+export function statAddClick(event, arr) {
+  arr.push({
+    type: event.type,
+    time: Date.now(),
+    x: event.x,
+    y: event.y,
+  });
+}
+
+export function addMultipleEventListener(element, events, handler) {
+  events.forEach((e) => element.addEventListener(e, handler));
+}
+//#endregion
+
+//#region Trash
 //#endregion
