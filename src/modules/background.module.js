38083f66c0edeb5110d63118dd5e25efd03e9703
Radko Ksenia <ksradko16@yandex.ru>
2022-06-25 12:42:39 +0300

    init class background

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,10 +1,17 @@
 import { Module } from '../core/module';
 
-console.log('123')
-console.log('hello')
+function getRandomColor () {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
 export class BackgroundModule extends Module {
   trigger() {
+    document.body.style.background = getRandomColor();
     console.log('trigger BackgroundModule');
   }
 }
