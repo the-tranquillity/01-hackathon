8e4da2587242f00b94bbe2994adb6e3986a74013
PavelKn <ferrari.inside@gmail.com>
2022-06-26 12:20:38 +0300

    fix square undefined

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -26,13 +26,16 @@ export class ShapeModule extends Module {
 
     const createRandomShape = ((shapeName) => {  
 
-      const getScreenSize = document.querySelector('body');
+      const wrapper = document.createElement('div');
+      wrapper.className = 'wrapper';
+      wrapper.style.filter = `drop-shadow(10px 10px 15px #00000099)`; 
       const shape = document.createElement('div');
-      const {width, height} = getScreenSize.getBoundingClientRect();
+      const windowInnerWidth = document.documentElement.clientWidth;
+      const windowInnerHeight = document.documentElement.clientHeight;
       const size = getRandomNumber(100, 300);
-      const y = getRandomNumber(0, width - size);
-      const x = getRandomNumber(0, height - size);
-      
+      const x = getRandomNumber(0, windowInnerHeight - size);
+      const y = getRandomNumber(0, windowInnerWidth - size);
+
       shape.classList.add(shapeName);
       shape.style.width = `${size}px`;
       shape.style.height = `${size}px`;
@@ -40,7 +43,9 @@ export class ShapeModule extends Module {
       shape.style.left = `${y}px`;
       shape.style.transform = `rotate(${getRandomNumber(0, 360)}deg)`;
       shape.backgroundColor = setColor(shape);
-      document.body.append(shape);
+      wrapper.append(shape)
+      document.body.append(wrapper);
+    
     });
 
     createRandomShape(figures[getRandomNumber(0, 6)]);
